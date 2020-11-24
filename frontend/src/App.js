6f6428542e9bfaa3fbb8ab5b1616e6c537a84532
Kranti Prasad <krantiprasad07@gmail.com>
2020-11-24 17:41:48 +0530

    added HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<HomeScreen />
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
