a4c3d53b77d53cbe8014364dc7151a8e36116f24
Kranti Prasad <krantiprasad07@gmail.com>
2020-12-16 22:52:22 +0530

    frontend setup

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,11 +6,11 @@ import Rating from "./Rating";
 const Product = ({ product }) => {
 	return (
 		<Card className="my-3 p-3 rounded">
-			<Link to={`/prducts/${product._id}`}>
+			<Link to={`/product/${product._id}`}>
 				<Card.Img src={product.image} variant="top" />
 			</Link>
 			<Card.Body>
-				<Link to={`/prducts/${product._id}`}>
+				<Link to={`/product/${product._id}`}>
 					<Card.Title as="div">
 						<strong>{product.name}</strong>
 					</Card.Title>
@@ -23,7 +23,7 @@ const Product = ({ product }) => {
 					/>
 				</Card.Text>
 
-				<Card.Text as="h3">&#x20B9;{product.price}</Card.Text>
+				<Card.Text as="h3">{product.price}</Card.Text>
 			</Card.Body>
 		</Card>
 	);
